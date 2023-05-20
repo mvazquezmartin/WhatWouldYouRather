556ea1eff79e863b07048b26b68a4c89ebd3b197
mvazquez <matias.vazquezmartin@gmail.com>
2023-05-20 03:54:50 -0300

    optmizacion rutas protegitas user, admin

--- src/middlewares/authorization.middleware.js
+++ src/middlewares/authorization.middleware.js
@@ -1,8 +1,8 @@
-const authorization = (role) => {
+const authorization = (roles) => {
   return async (req, res, next) => {
     if (!req.user) return res.status(401).json({ error: "Unauthorized" });
 
-    if (req.user.role !== role)
+    if (!roles.includes(req.user.role))
       return res.status(403).json({ error: "Forbidden" });
     next();
   };
